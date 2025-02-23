97d30a22cce8bd767d06feca0e53a5b9f4b15d6b
MattyMurdock <oswaldlazarof.99@gmail.com>
2025-02-23 13:00:47 -0600

    [ADD] #02 Agregando documentacion del archivo ingreso
    ES para poder saber que hace

--- PHP/ingreso2.php
+++ PHP/ingreso2.php
@@ -1,3 +1,27 @@
+/**
+ * Archivo: /home/lazaro/Documentos/Proyectos/ProyectoFinal Equipo7/Proyecto_InvernaTEC/PHP/ingreso2.php
+ * 
+ * Este script PHP recibe parámetros a través de la URL y los inserta en una base de datos.
+ * 
+ * Parámetros esperados:
+ * - usuario: Nombre del usuario (string)
+ * - actuador: Nombre del actuador (string)
+ * - marca: Marca del actuador (string)
+ * 
+ * Funcionalidad:
+ * 1. Establece la zona horaria a 'America/Mexico_City'.
+ * 2. Obtiene la fecha y hora actual.
+ * 3. Incluye el archivo de conexión a la base de datos 'conexion2.php'.
+ * 4. Inserta un nuevo registro en la tabla 'planta' con los datos proporcionados.
+ * 5. Muestra un mensaje indicando si la inserción fue exitosa o fallida.
+ * 
+ * Dependencias:
+ * - conexion2.php: Archivo que contiene la configuración y conexión a la base de datos.
+ * 
+ * Mensajes de salida:
+ * - "Registro insertado exitoso" si la inserción en la base de datos fue exitosa.
+ * - "Fallo en el registro" si hubo un error en la inserción.
+ */
 <?php
 $usuario=$_GET["usuario"];
 $actuador=$_GET["actuador"];
