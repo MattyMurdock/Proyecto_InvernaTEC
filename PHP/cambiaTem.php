80b723904cf42de5a9dc39ac31a398ab3de068ed
MattyMurdock <oswaldlazarof.99@gmail.com>
2025-02-23 12:29:13 -0600

    Agregando documentacion archivos cambio 2da parte
    Otra descripcion conchetumare

--- PHP/cambiaTem.php
+++ PHP/cambiaTem.php
@@ -1,4 +1,27 @@
-<?php
+/**
+ ** Este script PHP cambia el valor de la temperatura almacenado en un archivo de texto.
+ * 
+ * Parámetros de entrada:
+ * - valor (GET): El nuevo valor de la temperatura que se desea almacenar.
+ * 
+ ** Funcionalidad:
+ * 1. Obtiene el valor de la temperatura desde los parámetros GET.
+ * 2. Abre el archivo "Temperatura.txt" en modo de escritura.
+ * 3. Escribe el nuevo valor de la temperatura en el archivo.
+ * 4. Cierra el archivo.
+ * 5. Imprime un mensaje indicando que el valor ha sido cambiado.
+ * 
+ *? Archivo:
+ * - Temperatura.txt: Archivo donde se almacena el valor de la temperatura.
+ * 
+ * Ejemplo de uso:
+ * http://tu_dominio/cambiaTem.php?valor=25
+ * 
+ *! Advertencia:
+ * Asegúrese de que el archivo "Temperatura.txt" tenga los permisos adecuados para ser escrito por el servidor web.
+ */
+/***
+ <?php
 $valoractual = $_GET["valor"];
 $file = "Temperatura.txt";
 $fp = fopen($file, "w+");
--- PHP/cambiaVentilador.php
+++ PHP/cambiaVentilador.php
@@ -1,3 +1,29 @@
+/**
+ * Archivo: cambiaVentilador.php
+ * 
+ * Este script PHP recibe un valor a través de una solicitud GET y lo escribe en un archivo de texto llamado "ventilador.txt".
+ * 
+ * Parámetros de entrada:
+ * - valor (string): El valor que se desea escribir en el archivo "ventilador.txt".
+ * 
+ * Funcionalidad:
+ * 1. Obtiene el valor del parámetro "valor" de la solicitud GET.
+ * 2. Abre el archivo "ventilador.txt" en modo de escritura.
+ * 3. Escribe el valor obtenido en el archivo.
+ * 4. Cierra el archivo.
+ * 5. Imprime un mensaje indicando que el valor ha sido cambiado.
+ * 
+ * Salida:
+ * - Muestra el mensaje "valor cambiado" después de escribir en el archivo.
+ * 
+ * Uso:
+ * Este script se puede invocar mediante una URL con el parámetro "valor", por ejemplo:
+ * http://example.com/cambiaVentilador.php?valor=1
+ * 
+ * Notas:
+ * - Asegúrese de que el archivo "ventilador.txt" tenga los permisos adecuados para ser escrito por el servidor web.
+ * - Este script sobrescribirá el contenido existente en "ventilador.txt".
+ */
 <?php
 $valoractual = $_GET["valor"];
 $file = "ventilador.txt";
